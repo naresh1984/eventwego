34cb629f695e43227cdf01184437852e9223e716
naresh1984 <naresh.nookal@gmail.com>
2013-10-29 16:16:23 +0530

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+["Sports", "Music"].each do |r|
+  EventType.create(name: r)
+end
+
+["English", "Hindi", "Telugu"].each do |r|
+  EventLanguage.create(name: r)
+end
+
+
+
+employee = Event.create!(name: 'naresh', start_at: "2013-10-28", end_at: "2013-10-30",city:'hyd',event_type_id: '1',event_language_id:'1,2')
+
+
+
+
