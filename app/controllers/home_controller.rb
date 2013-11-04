f0820d06e3d74853df623d7256c2bd8ab2623600
naresh1984 <naresh.nookal@gmail.com>
2013-11-04 16:26:42 +0530

    m

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -124,18 +124,13 @@ end
      @nextmonth_count=nextmonth_count
      @all_events=all_events
      @all_types=all_types
-     @locount=all_location  
-    
-      
-    @typecount=@all_types.sum(:counts)
-    
- 
-    @locount=@locount.sum(:counts)
-
-   
+     @locount=all_location    
+     @typecount= @all_types.map { |h| h[:counts] }.sum
+     @locount=  @all_location.map { |h| h[:counts] }.sum  
+     
     
      end 
-   
+
   if params[:address].present?   
     results = Geocoder.search(params[:address])  
  @events=Event.joins(:event_type).select('events.id,events.name,events.start_at,events.end_at,events.start_hour,events.start_minute,events.address,events.avatar,event_types.name as type').where(@search).near([results[0].latitude,results[0].longitude],25).paginate(:page => params[:page],:per_page => 10).order("start_at ASC")
