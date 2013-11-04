1f489b782f5bc1b9b225da50fb1d5c81f4fe0ff8
naresh1984 <naresh.nookal@gmail.com>
2013-11-04 11:40:04 +0530

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ end
 @lan=EventLanguage.find(1)
 results = Geocoder.search('Hyderabad, Andhra Pradesh, India') 
 
-event = Event.create(name:'naresh', start_at: Date.today-1.days, end_at: Date.today-1.days, city: results[0].city , event_type_id: @type.id, event_language_id: @lan.id, latitude: results[0].latitude, longitude: results[0].longitude, state: results[0].state, country: results[0].country, zipcode: results[0].postal_code,start_minute: 00 ,start_hour: 9 ,start_meridiem: 'AM',end_minute: 00 ,end_hour: 11,end_meridiem: 'AM', address: 'Hyderabad, Andhra Pradesh, India',content:'hii',venue:'hyd' )
+event = Event.create(name:'naresh', start_at: Date.today, end_at: Date.today, city: results[0].city , event_type_id: @type.id, event_language_id: @lan.id, latitude: results[0].latitude, longitude: results[0].longitude, state: results[0].state, country: results[0].country, zipcode: results[0].postal_code,start_minute: 00 ,start_hour: 9 ,start_meridiem: 1,end_minute: 00 ,end_hour: 11,end_meridiem: 1, address: 'Hyderabad, Andhra Pradesh, India',content:'hii',venue:'hyd' )
 
 
 
