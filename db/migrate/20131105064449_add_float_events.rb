fa94cde1581d44479d19082364706dcf6dd85b31
naresh1984 <naresh.nookal@gmail.com>
2013-11-05 12:34:18 +0530

    first commit

--- db/migrate/20131105064449_add_float_events.rb
+++ db/migrate/20131105064449_add_float_events.rb
@@ -1,7 +1,10 @@
 class AddFloatEvents < ActiveRecord::Migration
   def up
- change_column :events, :latitude, :float
- change_column :events, :longitude, :float
+ remove_column :events, :latitude
+ remove_column :events, :longitude
+ add_column :events, :latitude, :float
+ add_column :events, :longitude, :float
+ 
   end
 
   def down
