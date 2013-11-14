1bd5fc6734a7f2cb261ca8e0fd0dda5b49d325b9
naresh1984 <naresh.nookal@gmail.com>
2013-11-14 14:21:30 +0530

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Eventwego::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-config.action_mailer.default_url_options = {:host => "stark-shore-8625.herokuapp.com/"}
+config.action_mailer.default_url_options = {:host => "polar-hamlet-5193.herokuapp.com"}
 config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
