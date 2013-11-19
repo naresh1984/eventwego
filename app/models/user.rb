ff6a5bc238197085caee572988707a665b9ead53
naresh1984 <naresh.nookal@gmail.com>
2013-11-19 09:53:44 +0530

    m

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,19 +20,19 @@ def self.from_omniauth(auth)
     #raise auth.credentials.token.inspect
     if user = User.find_by_email(auth.info.email)      
       user.provider = auth.provider
-      user.uid = auth.uid
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      #user.uid = auth.uid
+      #user.oauth_token = auth.credentials.token
+      #user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user
       
     else
       where(auth.slice(:provider, :uid)).first_or_create do |user|
         user.provider = auth.provider
-        user.uid = auth.uid
+        #user.uid = auth.uid
         #user.username = auth.info.name
         user.email = auth.info.email
-        user.oauth_token = auth.token
-        user.oauth_expires_at = auth.expires_at
+        #user.oauth_token = auth.token
+        #user.oauth_expires_at = auth.expires_at
         #user.password = 'nareshbabu'
         #user.avatar = auth.info.image
       end
